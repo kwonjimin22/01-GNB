74d1097bd3914b76cb42e7781301dd104517d615
jimin <jimin00study@gmail.com>
2023-11-29 11:56:43 +0900

    삼항조건 연산자 사용

--- 03/js/script.js
+++ 03/js/script.js
@@ -19,17 +19,18 @@ $(function () {
 
   // 마우스 휠을 조작했을 때 : wheel
   $window.on('wheel', function (e) {
-    // 마우스 휠을 조작했을 때의 이벤트 정보를 보자
-    // console.log(e);
-    // console.log(e.originalEvent.wheelDelta);
+    e.originalEvent.wheelDelta > 0 ? $header.removeClass('hide') : $header.addClass('hide');
 
-    // 마우스를 올리면 originalEvent.wheelDelta가 양수, 내리면 음수
-    if (e.originalEvent.wheelDelta > 0) {
-      // 휠을 올린 상태 --> header를 보이게하자 --> hide 클래스 제거
-      $header.removeClass('hide');
-    } else {
-      // 휠을 내린 상태 --> header를 숨기자 --> hide 클래스 부여
-      $header.addClass('hide');
-    }
+    // // 마우스 휠을 조작했을 때의 이벤트 정보를 보자
+    // // console.log(e);
+    // // console.log(e.originalEvent.wheelDelta);
+    // // 마우스를 올리면 originalEvent.wheelDelta가 양수, 내리면 음수
+    // if (e.originalEvent.wheelDelta > 0) {
+    //   // 휠을 올린 상태 --> header를 보이게하자 --> hide 클래스 제거
+    //   $header.removeClass('hide');
+    // } else {
+    //   // 휠을 내린 상태 --> header를 숨기자 --> hide 클래스 부여
+    //   $header.addClass('hide');
+    // }
   });
 });
